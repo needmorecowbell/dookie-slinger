11a40c71d55d72ec3e68c28dbfcb428dfa292522
needmorecowbell <amusciano@gmail.com>
2017-09-24 19:24:41 -0400

    works with multiple contacts, which means multiple emails

--- sling-the-dookie.py
+++ sling-the-dookie.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 
 import smtplib
+import json
 
-sender_email = 'amusciano@gmail.com' # insert user email
-sender_name= 'Adam Musciano'
-receivers = ['lightlytoasted33@gmail.com'] #insert company email
+with open('./res/config.json', 'r') as myfile:
+    config = json.loads(myfile.read())
 
-header = """
-From: From {var[sender_name]} <{var[sender_email]} >
+with open('./res/contacts/contacts.json') as myfile:
+    receivers = json.loads(myfile.read())
+    
+
+header = """From: From {var[sender_name]} <{var[sender_email]} >
 To: To {var[receiver_name]} <{var[receiver_email]}>
 Subject: {var[subject]}
 
@@ -17,25 +20,23 @@ with open('./res/templates/email.txt', 'r') as myfile:
         body = myfile.read()
 
 
-
 message= header+body
 
-
-for receiver in receivers:
+for receiver in receivers['companies']:
     custom_info = {
-            "sender_name":"Adam Musciano",
-            "sender_email":sender_email,
-            "receiver_name":'BossMan',
-            "receiver_email":receiver,
-            "company":"Rain Reality",
-            "job_title":"web developer",
-            "name":"Adam Musciano",
-            "subject":"Job Interest"
+            "receiver_name":receiver['name'],
+            "receiver_email":receiver['email'],
+            "company":receiver['company'],
+            "job":receiver['job'],
+            "subject":receiver['subject']
             }
+    data = config.copy()
+    data.update(custom_info) #append both dictionaries to data
+
+
 
     #insert templated text, replacing tags with config/contact info
-
-    print(message.format(var=custom_info))
+    print(message.format(var=data))
 
     try:
         smtpObj = smtplib.SMTP('smtp.gmail.com',587)
