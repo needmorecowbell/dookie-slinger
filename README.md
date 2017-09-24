d77c8aaec8339dd5b73ac3f919de4399ac29e785
needmorecowbell <amusciano@gmail.com>
2017-09-24 01:09:45 -0400

    Added django templating language

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Got some Dookie? **Sling it.*
+Got some Dookie? **Sling it.**
+
+This script will send a templated message out to any number of contacts. All you need to provide is a json file for the contacts and a text file with the templated messages
--- sling-the-dookie.py
+++ sling-the-dookie.py
@@ -1,24 +1,60 @@
 #!/usr/bin/python3
 
 import smtplib
+from django.template import Template, Context
 
+from django.conf import settings
+import django
+
+TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': './res/templates/email.txt'
+
+        }
+
+        ]
+
+settings.configure(TEMPLATES=TEMPLATES)
+
+django.setup()
 
 sender_email = 'from@gmail.com' # insert user email
 sender_name= 'Adam Musciano'
 receivers = ['to@todomain.com'] #insert company email
 
-message = """From: From ${sender_name} <${sender_email}>
-To: To ${receiver_name} <${receiver_email}>
-Subject: ${subject}
+header = """From: From {{sender_name}} <{{sender_email}} >
+To: To {{receiver_name}} <{{receiver_email}}>
+Subject: {{subject}}
 
-${body}
 """
 
-#insert templated text, replacing tags with config/contact info
+with open('./res/templates/email.txt', 'r') as myfile:
+        body = myfile.read()
+message= header+body
 
-try:
-   smtpObj = smtplib.SMTP('smtp.gmail.com',587)
-   smtpObj.sendmail(sender, receivers, message)         
-   print "Successfully sent email"
-except SMTPException:
-   print "Error: unable to send email"
+
+for receiver in receivers:
+    template = Template(message)
+    custom_info = {
+            "sender_name":"Adam Musciano",
+            "sender_email":sender_email,
+            "receiver_name":receiver,
+            "company":"Rain Reality",
+            "job_title":"web developer",
+            "name":"Adam Musciano"
+            }
+
+    #insert templated text, replacing tags with config/contact info
+
+    try:
+        smtpObj = smtplib.SMTP('smtp.gmail.com',587)
+       # smtpObj.sendmail(sender, receivers, template.render(Context(custom_info )))         
+        print("Full Message:\n"+ template.render(Context(custom_info)) )
+        print( "Successfully sent email")
+    except SMTPException:
+        print( "Error: unable to send email")
+
+
+
+
