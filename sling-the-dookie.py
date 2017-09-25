68f0f051de720f8bd8e9499d1f70be6513830d1d
needmorecowbell <amusciano@gmail.com>
2017-09-24 22:14:27 -0400

    Added progress bar and fixed smtp protocol

--- sling-the-dookie.py
+++ sling-the-dookie.py
@@ -2,49 +2,66 @@
 
 import smtplib
 import json
+import progressbar
 
 with open('./res/config.json', 'r') as myfile:
     config = json.loads(myfile.read())
 
-with open('./res/contacts/contacts.json') as myfile:
-    receivers = json.loads(myfile.read())
-    
+with open('./res/contacts/contacts.json','r') as myfile:
+    receivers = json.loads(myfile.read()) # call by using receivers['group']['item']
+ 
+with open('./res/templates/email.txt', 'r') as myfile:
+        body = myfile.read()
 
+
+#standard smtp protocol header template
 header = """From: From {var[sender_name]} <{var[sender_email]} >
 To: To {var[receiver_name]} <{var[receiver_email]}>
 Subject: {var[subject]}
 
 """
 
-with open('./res/templates/email.txt', 'r') as myfile:
-        body = myfile.read()
+message= header+body # append templated body from file to header
 
+#Setup mail
+smtpObj = smtplib.SMTP('smtp.gmail.com',587)
+smtpObj.ehlo()
+smtpObj.starttls()
+smtpObj.login(config['sender_email'], config['sender_password']) 
 
-message= header+body
+widgets = [
+        'Test:', progressbar.Percentage(),
+        'ETA: ', progressbar.ETA()
+        ]
 
-for receiver in receivers['companies']:
+bar = progressbar.ProgressBar(widgets= widgets, max_value= len(receivers['companies']))
+progressCount=0
+
+for receiver in receivers['companies']: #for every contact in contacts.json
     custom_info = {
             "receiver_name":receiver['name'],
             "receiver_email":receiver['email'],
             "company":receiver['company'],
             "job":receiver['job'],
             "subject":receiver['subject']
-            }
-    data = config.copy()
-    data.update(custom_info) #append both dictionaries to data
+            } #Adds all additional variables that might be in template
+    #add new key/val pairs to this dictionary for valid variables in formatting
+ 
+    data = config.copy()#make a copy of the config dictionary
+    data.update(custom_info) #append custom_info to final dictionary
 
-
-
-    #insert templated text, replacing tags with config/contact info
-    print(message.format(var=data))
+    message= message.format(var=data) #insert templated text, replacing tags with config/contact info
 
     try:
-        smtpObj = smtplib.SMTP('smtp.gmail.com',587)
-        #smtpObj.sendmail(sender, receivers, template.render(Context(custom_info )))         
-       # print("Full Message:\n"+ template.render(Context(custom_info)) )
-        print( "Successfully sent email")
+        smtpObj.sendmail(config['sender_email'], receiver['email'], message)
+        print("Full Message:\n"+message )
+        print( "\nSuccessfully sent email\n")
+        progressCount+=1
+        bar.update(progressCount)
+        print("\n**************************\n\n")
     except SMTPException:
         print( "Error: unable to send email")
 
-
+print("The dookies have all been slung!")
+bar.finish()
 
