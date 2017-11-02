a2d1e42c939de7e166e5ac9f98c905930eb1dc54
Adam Musciano <amusciano@gmail.com>
2017-11-02 14:53:27 -0400

    Added logo

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
+![dookie-logo](res/img/dookie.png)
 # The Dookie Slinger #
 
 Got some Dookie? **Sling it.**
 
 
 This script will send a templated message out to any number of contacts. All you need to provide is a json file for the contacts and a text file with the templated messages.
- 
+
 ## Dependencies ##
  * Python 3.5+
 
@@ -16,5 +17,3 @@ Installation is pretty easy...
 git clone https://github.com/needmorecowbell/dookie-slinger/
 sudo pip install -r requirements.txt
 ```
-
-
--- sling-the-dookie.py
+++ sling-the-dookie.py
@@ -21,7 +21,7 @@ Subject: {var[subject]}
 
 """
 
-message= header+body # append templated body from file to header
+message= header+body 
 
 #Setup mail
 smtpObj = smtplib.SMTP('smtp.gmail.com',587)
@@ -30,14 +30,14 @@ smtpObj.starttls()
 smtpObj.login(config['sender_email'], config['sender_password']) 
 
 widgets = [
-        'Test:', progressbar.Percentage(),
+        'Completed:', progressbar.Percentage(),
         'ETA: ', progressbar.ETA()
         ]
 
 bar = progressbar.ProgressBar(widgets= widgets, max_value= len(receivers['companies']))
 progressCount=0
 
-for receiver in receivers['companies']: #for every contact in contacts.json
+for receiver in receivers['companies']: 
     custom_info = {
             "receiver_name":receiver['name'],
             "receiver_email":receiver['email'],
@@ -47,20 +47,25 @@ for receiver in receivers['companies']: #for every contact in contacts.json
             } #Adds all additional variables that might be in template
     #add new key/val pairs to this dictionary for valid variables in formatting
  
-    data = config.copy()#make a copy of the config dictionary
+    data = config.copy()
     data.update(custom_info) #append custom_info to final dictionary
 
-    message= message.format(var=data) #insert templated text, replacing tags with config/contact info
 
     try:
-        smtpObj.sendmail(config['sender_email'], receiver['email'], message)
+        
         print("Full Message:\n"+message )
+        smtpObj.sendmail(config['sender_email'], receiver['email'], message.format(var=data))
+        
         print( "\nSuccessfully sent email\n")
+
         progressCount+=1
         bar.update(progressCount)
+
         print("\n**************************\n\n")
+
     except SMTPException:
         print( "Error: unable to send email")
+
 
 print("The dookies have all been slung!")
 bar.finish()
